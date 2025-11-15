3381680ee38518aeb99291224f605ea718a10f5c
Brad Thiessen <brthiess@ualberta.ca>
2025-11-14 22:20:10 -0700

    asdf

--- src/utilities/utility.ts
+++ src/utilities/utility.ts
@@ -3,6 +3,6 @@ import type { Team } from "@/router";
 export function getTeamPoints(team: Team): number {
   const regularPoints = team.wins * team.pointsPerWin;
   const playoffPoints = (team.playoffWins || 0) * team.pointsPerWin * 2;
-  const championBonus = team.isTournamentChampion ? 5 : 0;
+  const championBonus = team.isTournamentChampion ? 50 : 0;
   return regularPoints + playoffPoints + championBonus;
 }
